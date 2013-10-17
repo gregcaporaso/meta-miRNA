9df854c955d3347828c5c132f720a0d5bc73d863
Giorgio Casaburi <casaburi@ceinge.unina.it>
2013-10-18 00:50:58 +0200

    sra_to_qiime_pyqi.py updated

--- sra_to_qiiime.py
+++ sra_to_qiiime.py
@@ -11,58 +11,66 @@ __email__ = "casaburi@ceinge.unina.it"
 
 from pyqi.core.command import Command, Parameter, ParameterCollection
 
+from os import walk
+
+from pyqi.util import pyqi_system_call
+
 class SraToQiime(Command):
     BriefDescription = "post split libraries format: This script allows to convert .sra miRNA sequence data into a QIIME compatible format"
     LongDescription = "A script for converting SRA miRNA sequence data into a format that can be used with QIIME's closed reference OTU picking workflows. THIS CODE IS CURRENTLY UNTESTED. YOU SHOULD NOT USE THIS VERSION OF THE CODE. THIS MESSAGE WILL BE REMOVED WHEN TESTS ARE ADDED."
     Parameters = ParameterCollection([
         Parameter(Name='input_sra', DataType=str,
-                  Description='your input .sra file', Required=True),
-        Parameter(Name='output_dir', DataType=str,
-                  Description='the output directory', Required=True,
-                  Default=True)
+                  Description='your input should be a list of .sra formatted strings', Required=True),
+        Parameter(Name='output', DataType=str,
+                  Description='the output is a fasta string', Required=True)
     ])
 
 # sratoolkit and SCHIRMP are required to be installed by the User so that the tools sra_dumo and fastq_to_fasta can be called in the command line within the User $HOME.
 
-sra_dump_path = "$PATH/fastq-dump.2.3.1"
-fastq_to_fasta = "$PATH/fastq_to_fasta"
+    sra_dump_path = "/Users/giorgiocasaburi/Downloads/sratoolkit.2.3.1-mac64/bin/fastq-dump.2.3.1"
+    fastq_to_fasta = "/Users/giorgiocasaburi/SHRiMP_2_2_2/utils/fastq_to_fasta"
 
     def run(self, **kwargs):
+            
+        input_sra_folder = kwargs['input_sra']
+        if input_sra_folder[-1:] != "/":	
+            input_sra_folder = input_sra_folder + "/"  
+        input_filepaths = [] 
         
+        for (dirpath, dirnames, filenames) in walk(input_sra_folder):
+            input_filepaths.extend(filenames)
+            break
+        create_dir = kwargs['output']
     
-    
-    input_filepaths = Kwargs['input_sra']
-    
-    create_dir = Kwargs['output_dir']
-    
-    output_dir = Kwargs['output_dir']
+        output_dir_1 = kwargs['output']
 
 
-    for input_filepath in input_filepaths:
-        output_filepath = join(output_dir, input_filepath + ".fastq")
-      command = "%s %s -O %s" % (sra_dump_path, input_filepath, output_filepath)
+        for input_filepath in input_filepaths:
+            print input_filepath
+            output_filepath = output_dir_1 + input_filepath + ".fastq"
+            command = "%s %s -O %s" % (self.sra_dump_path, input_sra_folder + input_filepath, output_filepath)
 
-     stdout, stderr, ret_val = pyqi_system_call(command)
-     command = "rm %s" % (input_filepath)
+            stdout, stderr, ret_val = pyqi_system_call(command)
+            print stdout, stderr, ret_val
+            command = "rm %s" % (input_filepath)
 
-     stdout, stderr, ret_val = pyqi_system_call(command)
+            stdout, stderr, ret_val = pyqi_system_call(command)
 
-     command = "%s %s/*.fastq > %s.fast" % (fastq_to_fasta, output_filepath, output_filepath)
+            command = "%s %s/*.fastq > %s.fast" % (self.fastq_to_fasta, output_filepath, output_filepath)
 
-     stdout, stderr, ret_val = pyqi_system_call(command)
-     command = "rm %s/*.fastq" % (output_filepath)
+            stdout, stderr, ret_val = pyqi_system_call(command)
+            command = "rm -r %s" % (output_filepath)
 
-     stdout, stderr, ret_val = pyqi_system_call(command)
-     command = "sed 's/\./_/g;s/ .*$//g' %s*.fast > %s.fasta" % (output_filepath, output_filepath)
+            stdout, stderr, ret_val = pyqi_system_call(command)
+            command = "sed 's/\./_/g;s/ .*$//g' %s*.fast > %s.fasta" % (output_filepath, output_filepath)
 
-     stdout, stderr, ret_val = pyqi_system_call(command)
-        command = "rm %s*.fast" % (input_filepath)
+            stdout, stderr, ret_val = pyqi_system_call(command)
+            command = "rm %s*.fast" % (input_filepath)
 
-     stdout, stderr, ret_val = pyqi_system_call(command)
-    
+            stdout, stderr, ret_val = pyqi_system_call(command)
+        
+        return {'result': "sra file converted in fasta"}
 
-CommandConstructor = sra_to_qiime
-
-
+CommandConstructor = SraToQiime
 
 
